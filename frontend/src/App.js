18a168998ec6bc700f038cd0dd1c3768dc101a6b
shaima96 <141062@ppu.edu.ps>
2020-11-14 20:00:25 +0200

    Connect home page with login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Login from '../src/components/LogIn/LogIn.js'
 import SignUp from '../src/components/Signup/signUp.js';
 import Home from '../src/components/Home/home.js'
 
 
 class App extends Component {
-    constructor() {
-        super();
-
-    }
+    // constructor() {
+    //     super();
 
     render() {
         return (
-            <Router>
-                <App>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/login" component={Login} />
-                </App>
-            </Router>
+            // <Router>
+            //     <App>
+            //         {/* <Route path="/" exact component={Home} /> */}
+            //         <Route path="/login" component={Login} />
+            //     </App>
+            // </Router>
+            <div className="App">
+                <Router>
+                    <Route path="/" exact component={() => <Home />} />
+                    {/* <Redirect to="/login" /> */}
+                    <Route path="/login" exact component={() => <Login />} />
+                    <Route path="/signup" exact component={() => <SignUp />} />
+                </Router>
+            </div>
         );
     }
 }
--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LogIn from '../LogIn/LogIn.js'
-import axios from 'axios'
-const Home = ({ history }) => {
-    return (<div><h3>FoodDose</h3>
-        <button onClick={() => {
-            history.push("/LogIn")
-        }}>Login</button>
-    </div>
-    )
-}
-// class Home extends Component {
-//     constructor() {
-//         super();
-
-//     }
+// import Login from '../src/components/LogIn/LogIn.js'
+// import axios from 'axios'
+// import { browserHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 
 
-//     render() {
-//         return (
-//             <div className='container'>
-//               <div className="hreader">
-//                 <h3>FoodDose</h3>
-//                 <button onClick={()=>{
-//                     history.push("/login")
-//                 }}>Login</button>
-//               </div>
+// const Home = ({ history }) => {
 
-//             </div>
-//         );
-//     }
+//     return (
+//     <div>
+//         <h3>FoodDose</h3>
+//         <button onClick={() => {
+//             browserHistory.push("/login")
+//         }}>Login</button>
+//     </div>
+//     )
+
 // }
+function Home() {
+    // home = () => {
+    //     browserHistory.push("/Login");
+    //       };
+
+    let history = useHistory();
+    return (
+        <div className='container'>
+            <div className="header">
+                <h3>FoodDose</h3>
+                <button onClick={() => {
+                    history.push('/login')
+                }}>Login</button>
+            </div>
+        </div>
+    );
+}
 
 
 export default Home;
