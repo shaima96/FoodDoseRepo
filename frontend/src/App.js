2875aa823cba63def10e227d50e65f038ece56c9
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-15 20:23:49 +0200

    added fastresturrant component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Footer from '../src/components/Footer/footer.js';
 import Login from '../src/components/LogIn/LogIn.js'
 import SignUp from '../src/components/Signup/signUp.js';
 import Home from '../src/components/Home/home.js';
+
 
 
 
@@ -14,8 +16,8 @@ class App extends Component {
                     <Route path="/" exact component={() => <Home />} />
                     <Route path="/login" exact component={() => <Login />} />
                     <Route path="/signup" exact component={() => <SignUp />} />
-
                 </Router>
+                <Footer />
             </div>
         );
     }
