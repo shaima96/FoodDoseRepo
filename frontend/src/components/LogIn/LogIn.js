3705f22d9526088a07ab608270928443d23dd2e4
NajwanQandeel <najwan.qandeel@gmail.com>
2020-11-15 17:32:39 +0200

    signup in and login front end

--- frontend/src/components/LogIn/LogIn.js
+++ frontend/src/components/LogIn/LogIn.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'
+import axios from 'axios';
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import Footer from '../Footer/footer.js';
+import { TextField, Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import './LogIn.css'
 
 class Login extends Component {
     constructor() {
@@ -38,18 +44,49 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                <div className='container'>
-                    <div className='form-div'>
-                        <form onSubmit={this.onSubmit}>
-                            <input type='text' placeholder='Email' onChange={this.changeEmail} value={this.state.Email} className='form-control form-group' />
-                            <input type='password' placeholder='Password' onChange={this.changePassword} value={this.state.Password} className='form-control form-group' />
-                            <input type='submit' className='btn btn-danger btn-block' value='Login' />
-                        </form>
-                    </div>
+        
+            <div >
+            <Typography component="h1" variant="h3" align="center" id="title"> Login</Typography><br />
+            <Box display="flex" justifyContent="center" alignItems="center" minHeight="40vh">
 
-                </div>
-            </div>
+                <form onSubmit={this.onSubmit} >
+                <FormControl margin="normal" required >
+                        <TextField
+                            id="email"
+                            name="email"
+                            type="email"
+                            value={this.state.Email}
+                            onChange={this.changeEmail}
+                            label="Email" variant="outlined"
+                        />
+                    </FormControl><br />
+                    <FormControl margin="normal" required>
+                        <TextField
+                            id="password"
+                            name="password"
+                            type="Password"
+                            value={this.state.Password}
+                            onChange={this.changePassword}
+                            label="Password" variant="outlined"
+                        />
+                    </FormControl><br /><br />
+                   
+                    <Button id="btn"
+                        onClick={this.handleSubmit}
+                        type="submit"
+                        size="large"
+                        variant="contained"
+                        justifyContent="center"
+
+
+                    >
+                       Login
+          </Button>
+                </form>
+
+            </Box>
+            <Footer />
+        </div >
         );
     }
 }
